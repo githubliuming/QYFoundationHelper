87bd7befbc03eaa30096261cd81af111c7c7b5ce
liuming <1969489778@qq.com>
2017-02-09 14:36:59 +0800

    添加对url地址编码

--- QYFoundationHelper/Category/NSString/NSString+URLHandler.h
+++ QYFoundationHelper/Category/NSString/NSString+URLHandler.h
@@ -23,4 +23,44 @@
  *  验证网络地址格式
  */
 - (BOOL)isValidUrl;
+
+#pragma mark 网络地址编码
+/**
+ *  @brief  urlEncode
+ *
+ *  @return urlEncode 后的字符串
+ */
+- (NSString *)urlEncode;
+
+/**
+ *  @brief  urlEncode
+ *
+ *  @param encoding encoding模式  ios9以后默认 uft-8
+ *
+ *  @return urlEncode 后的字符串
+ */
+- (NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding;
+/**
+ *  @brief  urlDecode
+ *
+ *  @return urlDecode 后的字符串
+ */
+- (NSString *)urlDecode;
+
+/**
+ *  @brief  urlDecode
+ *
+ *  @param encoding encoding模式 ios9以后默认 uft-8
+ *
+ *  @return urlDecode 后的字符串
+ */
+- (NSString *)urlDecodeUsingEncoding:(NSStringEncoding)encoding;
+
+/**
+ *  @brief  url query转成NSDictionary
+ *
+ *  @return NSDictionary
+ */
+- (NSDictionary *)dictionaryFromURLParameters;
 @end
+
--- QYFoundationHelper/Category/NSString/NSString+URLHandler.m
+++ QYFoundationHelper/Category/NSString/NSString+URLHandler.m
@@ -7,7 +7,7 @@
 //
 
 #import "NSString+URLHandler.h"
-
+#import <UIKit/UIKit.h>
 @implementation NSString (URLHandler)
 
 - (NSURL *)convertToURL
@@ -34,5 +34,86 @@
     NSPredicate *urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
     return [urlTest evaluateWithObject:self];
 }
+
+- (NSString *)urlEncode { return [self urlEncodeUsingEncoding:NSUTF8StringEncoding]; }
+- (NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding
+{
+    if (IOS_SYSTEM_VERSION >= 9.0)
+    {
+        NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"!*'\"();:@&=+$,/?%#[]% "];
+        return [self stringByAddingPercentEncodingWithAllowedCharacters:set];
+    }
+    else
+    {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        return (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(
+            NULL, (__bridge CFStringRef)self, NULL, (CFStringRef) @"!*'\"();:@&=+$,/?%#[]% ",
+            CFStringConvertNSStringEncodingToEncoding(encoding));
+#pragma clang diagnostic pop
+    }
+}
+
+/**
+ *  @brief  urlDecode
+ *
+ *  @return urlDecode 后的字符串
+ */
+- (NSString *)urlDecode {
+    return [self urlDecodeUsingEncoding:NSUTF8StringEncoding];
+}
+/**
+ *  @brief  urlDecode
+ *
+ *  @param encoding encoding模式
+ *
+ *  @return urlDecode 后的字符串
+ */
+- (NSString *)urlDecodeUsingEncoding:(NSStringEncoding)encoding
+{
+    if (IOS_SYSTEM_VERSION >= 9.0)
+    {
+        return (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapes(
+            NULL, (__bridge CFStringRef)self, CFSTR(""));
+    }
+    else
+    {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        return (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(
+            NULL, (__bridge CFStringRef)self, CFSTR(""), CFStringConvertNSStringEncodingToEncoding(encoding));
+#pragma clang diagnostic pop
+    }
+}
+
+/**
+ *  @brief  url query转成NSDictionary
+ *
+ *  @return NSDictionary
+ */
+- (NSDictionary *)dictionaryFromURLParameters
+{
+    NSArray *pairs = [self componentsSeparatedByString:@"&"];
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    for (NSString *pair in pairs)
+    {
+        NSArray *kv = [pair componentsSeparatedByString:@"="];
+        NSString *val = @"";
+        if (IOS_SYSTEM_VERSION >= 9.0)
+        {
+            val = [[kv objectAtIndex:1] stringByRemovingPercentEncoding];
+        }
+        else
+        {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            val = [[kv objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+#pragma clang diagnostic pop
+        }
+        [params setObject:val forKey:[kv objectAtIndex:0]];
+    }
+    return params;
+}
+
 @end
 
